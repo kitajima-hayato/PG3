cd31bb2157258b6d48b72491cfd75b2dbe31aa81
kitajima-hayato <k023g0057@g.neec.ac.jp>
2024-11-13 23:31:34 +0900

    AnimalMove確認
    動物クラスの追加とmain.cppの更新
    
    - `PG3.vcxproj`に`Animal.cpp`, `Lion.cpp`, `Tiger.cpp`のソースファイルと`Animal.h`, `Lion.h`, `Tiger.h`のヘッダーファイルを追加
    - `main.cpp`からテンプレートクラスとその使用例を削除し、`Animal.h`, `Lion.h`, `Tiger.h`をインクルード
    - `main.cpp`で`std::vector`を使用して`Lion`と`Tiger`のオブジェクトを作成し、`Move`メソッドを呼び出すコードを追加
    - `Animal.cpp`に`Animal`クラスの`Move`メソッドを実装
    - `Animal.h`に`Animal`クラスの宣言と仮想関数`Move`を追加
    - `Lion.cpp`に`Lion`クラスの`Move`メソッドを実装
    - `Lion.h`に`Lion`クラスの宣言と`Move`メソッドのオーバーライドを追加
    - `Tiger.cpp`に`Tiger`クラスの`Move`メソッドを実装
    - `Tiger.h`に`Tiger`クラスの宣言と`Move`メソッドのオーバーライドを追加

--- main.cpp
+++ main.cpp
@@ -1,49 +1,21 @@
 #include <iostream>
-using namespace std;
-
-template <typename T>
-class MinClass {
-public:
-    T Min(T a, T b) {
-        return (a < b) ? a : b;
-    }
-};
-
-
-// 2つの型引数を持つクラステンプレートの定義
-template <typename T1, typename T2>
-class MinClass2 {
-public:
-    // 2つの引数の小さい方の値を返すメンバ関数
-    auto Min(T1 a, T2 b) -> decltype((a < b) ? a : b) {
-        return (a < b) ? a : b;
-    }
-};
-
-int main() {
-    // int
-    MinClass<int> intIntMin;
-    cout << "int, intの最小値: " << intIntMin.Min(4, 33) << endl;
-
-    // float
-    MinClass<float> floatFloatMin;
-    cout << "float, floatの最小値: " << floatFloatMin.Min(3.3f, 33.4f) << endl;
-
-    // double, doubleの組み合わせ
-    MinClass<double> doubleDoubleMin;
-    cout << "double, doubleの最小値: " << doubleDoubleMin.Min(4.33, 33.4) << endl;
-
-    // int, float
-    MinClass2<int, float> intFloatMin;
-    cout << "int, floatの最小値: " << intFloatMin.Min(3, 33.4f) << endl;
-
-    // int, double
-    MinClass2<int, double> intDoubleMin;
-    cout << "int, doubleの最小値: " << intDoubleMin.Min(3, 33.4) << endl;
-
-    // float, double
-    MinClass2<float, double> floatDoubleMin;
-    cout << "float, doubleの最小値: " << floatDoubleMin.Min(4.4f, 33.4) << endl;
-
-    return 0;
+#include <vector>
+#include "Animal.h"
+#include "Lion.h"
+#include "Tiger.h"
+int main()
+{
+	std::vector<Animal*> animals;
+	animals.push_back(new Lion());
+	animals.push_back(new Tiger());
+	for (auto animal : animals)
+	{
+		animal->Move();
+	}
+	for (auto animal : animals)
+	{
+		delete animal;
+	}
+	animals.clear();
+	return 0;
 }
