10a0429e74fcd3864218f80f124f339c683921d9
kitajima-hayato <k023g0057@g.neec.ac.jp>
2024-11-29 21:28:38 +0900

    確認課題完了

--- main.cpp
+++ main.cpp
@@ -1,51 +1,42 @@
+#include <fstream>
+#include <vector>
+#include <cstring>
 #include <iostream>
-#include <list>
-#include <cstring> 
-
+#include <string> 
+#include <algorithm>
+#include <sstream>
 using namespace std;
 
-void displayList(const list<const char*>& stations) {
-    for (const auto& station : stations) {
-        cout << station << " ";
-    }
-    cout << endl;
-}
-
 int main() {
+    vector<string> numbers;
+    ifstream file("File/PG3_2024_03_02.txt");
 
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            stringstream ss(line);
+            string item;
+            while (getline(ss, item, ',')) {
+                numbers.push_back(item);
+            }
+        }
+        file.close();
+    }
+    else {
+        cout << "Unable to open file" << endl;
+        return 1;
+    }
 
-    // 駅名リスト（英語表記）
-    list<const char*> stations_1970s = {
-        "Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani",
-        "Nippori", "Tabata", "Komagome", "Sugamo", "Otsuka", "Ikebukuro",
-        "Mejiro", "Takadanobaba", "Shin-Okubo", "Shinjuku", "Yoyogi", "Harajuku",
-        "Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki", "Shinagawa",
-        "Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
-    };
+    // 文字列を昇順に並び替え
+    sort(numbers.begin(), numbers.end());
 
-    
-    auto it = stations_1970s.begin();
-    advance(it, 8); 
-    stations_1970s.insert(it, "Nishi-Nippori");
-
-    
-    list<const char*> stations_2019 = stations_1970s;
-
-    
-    list<const char*> stations_2022 = stations_2019;
-    it = stations_2022.begin();
-    advance(it, 22); // Shinagawaの後に追加
-    stations_2022.insert(it, "Takanawa Gateway");
-
-    // 駅リストを表示
-    cout << "Stations in 1970s:" << endl;
-    displayList(stations_1970s);
-
-    cout << "\nStations in 2019:" << endl;
-    displayList(stations_2019);
-
-    cout << "\nStations in 2022:" << endl;
-    displayList(stations_2022);
+    // 並び替えた内容をコンソールに出力
+    for (const auto& number : numbers) {
+        cout << number << endl;
+    }
 
     return 0;
 }
+
+
+
